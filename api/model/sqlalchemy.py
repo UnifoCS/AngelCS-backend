cb357e954d2394e63fe51190feaaf7849bdd6483
HeegyuKim <heekue83@gmail.com>
2019-10-06 19:15:37 +0900

    sqlalchemy model에 주석 추가

--- api/model/sqlalchemy.py
+++ api/model/sqlalchemy.py
@@ -17,21 +17,34 @@ class BaseModel():
 
     @declared_attr
     def __tablename__(cls):
+        """
+        테이블명은 Wrapper 클래스의 이름을 snake_case로 바꾸도록 만드는 함수.
+        """
         return convert_camel_to_snake(cls.__name__)
 
 
 
 class IdMixin():
+    """
+    이 클래스를 상속하는 래퍼 클래스는 ID 필드를 갖게됨.
+    """
     id =  Column(Integer, primary_key=True, autoincrement=True)
 
 
 class TimestampMixin():
+    """
+    이 클래스를 상속하는 래퍼 클래스는 created_date(생성시간), updated_date(마지막 변경시간) 필드를 갖게됨.
+    updated_date는 레코드의 다른 값이 바뀌면 바로 변경됨.
+    """
     created_date = Column(DateTime, default=datetime.utcnow())
     updated_date = Column(DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow())
 
 
 
 class User(Base, BaseModel, IdMixin, TimestampMixin):
+    """
+    AngelCS를 사용하는 유저를 나타내는 테이블.
+    """
     name = Column(String(32), nullable=False)
     email = Column(String(64), nullable=False, index=True)
     password_hash = Column(String(128), nullable=False)
@@ -60,12 +73,18 @@ class User(Base, BaseModel, IdMixin, TimestampMixin):
 
 
 class ReferenceUserMixin:
+    """
+    한 유저에 종속되어 1-to-many관계를 갖는 테이블을 위한 믹스인 클래스
+    """
     @declared_attr
     def user_id(cls):
         return Column(Integer, ForeignKey("user.id"), index=True, nullable=False)
 
 
 class Channel(Base, BaseModel, IdMixin, TimestampMixin):
+    """
+    채널은 유저가 리뷰를 가져오는 곳을 의미함(유튜브, 플레이스토어, 쿠팡 등)
+    """
     name = Column(String(64), nullable=False)
     provider = Column(String(64), nullable=False)
 
@@ -89,12 +108,18 @@ class Channel(Base, BaseModel, IdMixin, TimestampMixin):
 
 
 class ReferenceChannelMixin():
+    """
+    하나의 채널에 종속된 1-to-many를 나타내기 위한 믹스인 클래스.
+    """
     @declared_attr
     def channel_id(cls):
         return Column(Integer, ForeignKey("channel.id"), index=True, nullable=False)
 
 
 class LinkUserChannel(Base, BaseModel, IdMixin):
+    """
+    유저와 채널간의 many-to-many 관계를 표현하기 위한 클래스.
+    """
     user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
     channel_id = Column(Integer, ForeignKey('channel.id'), nullable=False)
     __table_args__ = (
@@ -103,6 +128,9 @@ class LinkUserChannel(Base, BaseModel, IdMixin):
 
 
 class Review(Base, BaseModel, IdMixin, TimestampMixin, ReferenceChannelMixin):
+    """
+    리뷰 테이블을 나타내는 클래스로, 하나의 채널에 종속된다.
+    """
     title = Column(String(64), nullable=False)
     author = Column(String(32), nullable=False)
     content = Column(String(1024), nullable=False)
@@ -133,6 +161,9 @@ class Review(Base, BaseModel, IdMixin, TimestampMixin, ReferenceChannelMixin):
         return d
 
 class ReferenceReviewMixin():
+    """
+    하나의 리뷰에 종속되어 1-to-many관계를 갖는 클래스를 위한 믹스인 클래스.
+    """
     @declared_attr
     def review_id(cls):
         return Column(Integer, ForeignKey("review.id"), index=True)
@@ -140,6 +171,10 @@ class ReferenceReviewMixin():
 
 
 class Tag(Base, BaseModel, IdMixin, TimestampMixin):
+    """
+    리뷰의 태그를 나타내는 클래스. 
+    Review와 many-to-many관계를 가짐.
+    """
     name = Column(String(32), nullable=False)
     
     reviews = relationship('Tag', secondary='link_review_tag')
@@ -151,6 +186,9 @@ class Tag(Base, BaseModel, IdMixin, TimestampMixin):
         }
 
 class LinkReviewTag(Base, BaseModel, IdMixin):
+    """
+    SQLAlchemy에서 리뷰와 태그를 many-to-many로 연결하기 위한 클래스
+    """
     review_id = Column(Integer, ForeignKey('review.id'), nullable=False)
     tag_id = Column(Integer, ForeignKey('tag.id'), nullable=False)
 
@@ -161,6 +199,10 @@ class LinkReviewTag(Base, BaseModel, IdMixin):
 
 
 class Template(Base, BaseModel, IdMixin, TimestampMixin, ReferenceChannelMixin):
+    """
+    템플릿 테이블을 나타내기 위한 클래스. 
+    0개 이상의 TemplateCondition을 가짐.
+    """
     name = Column(String(32), nullable=False)
     content = Column(String, nullable=False)
 
@@ -180,11 +222,19 @@ class Template(Base, BaseModel, IdMixin, TimestampMixin, ReferenceChannelMixin):
         return d
 
 class ReferenceTemplateMixin():
+    """
+    하나의 템플릿에 종속되어 1-to-many관계를 가진 테이블을 나타내기위한 믹스인 클래스.
+    """
+
     @declared_attr
     def template_id(cls):
         return Column(Integer, ForeignKey("template.id"), index=True, nullable=False)
 
 class TemplateCondition(Base, BaseModel, IdMixin, ReferenceTemplateMixin):
+    """
+    Template이 리뷰에 적용되기 위한 조건이 나열된 클래스.
+    TemplateCondition은 하나의 template에 여러개가 연결된 1-to-many
+    """
     index = Column(Integer, index=True)
     operand1 = Column(String)
     operand2 = Column(String)
