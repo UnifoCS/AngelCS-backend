ceeb5708f6113180b928ab859697597b5cf4a1d2
HeegyuKim <heekue83@gmail.com>
2019-10-28 22:44:30 +0900

    템플릿 추천기능 구현, 테스트트X

--- api/service/template_service.py
+++ api/service/template_service.py
@@ -13,6 +13,57 @@ class TemplateService(BaseDatabaseService):
         templates = self.query(Template).all()
         return templates
 
+    def recommendate_template(self, reviews):
+        templates = self.get_all_templates()
+        recommends = []
+
+        for review in reviews:
+            candidates = []
+
+            review_tag_ids = [str(t.id) for t in review.tags]
+            ok = 0
+            score = 0
+
+            for t in templates:
+
+                for tc in templates.conds:
+                    if tc.operator == "tag_in":
+                        tag_ids = t.operand2.split(",")
+                        score = score + sum([x in review_tag_ids for x in tag_ids])
+                        ok = ok + 1
+
+                    elif tc.operator == "keyword_in":
+                        keywords = t.operand2.split(",")
+                        score = score + sum([k in review.content for k in keywords])
+                        ok = ok + 1
+
+                    if tc.operand1 == "rating":
+                        score = score + 1
+                        result = False 
+
+                        if tc.operator == "=":
+                            result = review.rating == int(t.operand2)
+                        elif tc.operator == ">":
+                            result = review.rating > int(t.operand2)
+                        elif tc.operator == ">=":
+                            result = review.rating >= int(t.operand2)
+                        elif tc.operator == "<":
+                            result = review.rating < int(t.operand2)
+                        elif tc.operator == "<=":
+                            result = review.rating <= int(t.operand2)
+                        
+                        if result:
+                            ok = ok + 1
+            
+                # template conditions 을 모두 만족해야함. ok 로 카운팅
+                # score는 점수
+                if score > 0 and ok == len(t.conds):
+                    candidates.append((t, score))
+                
+            candidates = sorted(candidates, lambda x: x[1])
+            recommends.append(candidates)
+
+        return recommends
 
     def get_template_by_tags(self, tags):
         if not tags:
